65d5e0aa5e65edf7c5fc78f29af1dfb262f0b60b
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2025-05-23 14:53:41 +0700

    Added params

--- src/utils/pollination.py
+++ src/utils/pollination.py
@@ -2,6 +2,18 @@ import aiohttp
 import asyncio
 import urllib.parse
 
+params = {
+    "model": "turbo",
+    "seed": 123,
+    "width": 720,
+    "height": 1280,
+    "nologo": "true",
+    "private": "true",
+    "enhance": "false",
+    "safe": "false",
+    "referrer": "ImageGenTest"
+}
+
 async def fetch_image(prompt: str, filename: str = "temp.jpg", **params):
     encoded_prompt = urllib.parse.quote(prompt)
     url = f"https://image.pollinations.ai/prompt/{encoded_prompt}"
@@ -24,3 +36,15 @@ async def fetch_image(prompt: str, filename: str = "temp.jpg", **params):
                     return False
     except Exception as e:
         print(f"Error: {e}")
+
+
+async def main():
+    prompt = "1girl, cute, cat girl, maid, large breast, nipples, nsfw, close up"
+    success = await fetch_image(prompt, "output.jpg", **params)
+    if success:
+        print("Image downloaded successfully.")
+    else:
+        print("Image download failed.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
